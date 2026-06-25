954de288c075962d82fad4323ef710ae4949a70c
vagile-devsecops <vagile1001@gmail.com>
2026-06-25 08:37:35 +0530

    Add Terraform configuration for Google Cloud VM

---
+++ gcp.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "YOUR_PROJECT_ID"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "terraform-vm"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+}
